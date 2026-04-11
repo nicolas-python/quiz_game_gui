5f3f7bf1177dc1c1e6248bb3751d680023776b94
Nicolas <nicolasmader001@gmail.com>
2026-04-11 16:01:38 +0200

    add quiz questions

--- main.py
+++ main.py
@@ -49,6 +49,74 @@ class quiz_game:
         self.button_exit = tkinter.Button(self.frame_buttons, text="Beenden", command=self.exit_game)
         self.button_exit.pack(pady=10)
 
+        #fragen
+        self.question = [
+            #Grundfunktionen
+            ("Was macht int()?", "Ganzzahl"),           #Wert wird in eine Ganzzahl umgewandelt
+            ("Was macht str()?", "Text"),               #Wandelt alles in text um 5 wird zu"5"
+            ("Was ist bool?", "True/False"),            #Datentyp für Wahr oder Falsch
+            ("Was macht float()?", "Kommazahl"),        #wandelt in Kommazahl um (5 zu 5.0)
+            ("Was macht import?", "lädt Modul"),             #lädt externe Module/Bibliotheken
+            ("Was ist print()?", "Ausgabe"),            #gibt Text aus
+            ("Was ist input()?", "Eingabe"),            #Benutzer schreibt etwas
+            #funktionen
+            ("Was macht def?", "Funktion"),             #erstellt eine eigene Funktion
+            ("Was macht return?", "Rückgabe"),          #gibt ein Ergebnis aus einer Funktion zurück
+            ("Was ist Parameter?", "Wert"),             #wert, der einer Funktion übergeben wird
+            #Listen & Text
+            ("Was macht len()?", "Länge"),              #zählt Zeichen oder Elemente
+            ("Was macht split()?", "Trennen"),          #trennt Text in einzelne Teile (Liste)
+            ("Was macht .strip()?", "entfernt Leerzeichen"),  #entfernt Leerzeichen am Anfang/Ende (\n wäre Zeilenumbruch)
+            ("Was macht .lower()?", "klein"),           #macht alles klein
+            ("Was macht .upper()?", "gross"),           #macht alles groß
+            #Listen Funktionen
+            ("Was macht append()?", "Hinzufügen"),      #fügt ein Element ans Ende der Liste
+            ("Was macht remove()?", "Löschen"),         #entfernt ein bestimmtes Element
+            ("Was macht pop()?", "Letztes"),            #entfernt das !letzte! Element
+            ("Was macht sort()?", "Sortieren"),         #sortiert eine Liste
+            ("Was macht []?", "Liste"),                 #speichert mehrere Werte in einer Sammlung
+            ("Was macht index?", "Position"),           #gibt die Position eines Elements in einer Liste zurück
+            ("Was macht in?", "Enthalten"),             #prüft ob etwas in einer Liste ist
+            #Schleifen / range
+            ("Was macht for?", "Schleife"),             #wiederholt Code eine bestimmte Anzahl
+            ("Was macht while?", "Wiederholung"),       #wiederholt solange eine Bedingung stimmt
+            ("Was macht break?", "Stop"),               #beendet eine Schleife sofort
+            ("Was macht continue?", "Weiter"),          #überspringt einen Schritt
+            ("Was macht range(5)?", "Zählen"),          #erzeugt eine Zahlenreihe (Start bei 0)
+            ("Was gibt range(5) zurück?", "0-4"),       #Liste von Zahlen 0 bis 4
+            ("Wofür nutzt man range()?", "Wiederholung"), #für Wiederholungen in for-Schleifen
+            #Dateien
+            ("Was macht open()?", "Datei"),             ## öffnet oder erstellt eine Datei
+            ("Was macht open('r')?", "Lesen"),          #öffnet Datei zum Lesen
+            ("Was macht open('w')?", "Schreiben"),      #öffnet Datei zum Schreiben
+            #Bedingungen
+            ("Was macht if?", "Bedingung"),             #prüft ob die bedingung wahr ist
+            ("Was macht else?", "Sonst"),               #alternative bedingungen wenn if falsch ist
+            ("Was macht elif?", "Sonst-wenn"),          #weitere Bedingung wenn if falsch ist
+            ("Was macht ==?", "Gleich"),                #prüft ob zwei Werte gleich sind
+            ("Was macht !=?", "Ungleich"),              #prüft ob Werte ungleich sind
+            ("Was macht isalpha()?", "Buchstaben"),     #prüft ob nur Buchstaben vorhanden sind
+            ("Was macht isdigit()?", "Zahlen"),         #prüft ob nur Zahlen vorhanden sind
+            ("Was prüft ==?","Vergleich"),              #vergleicht zwei Werte
+            ("Was macht < ?", "kleiner"),               #kleiner als
+            ("Was macht > ?", "größer"),                #größer als
+            #Rechnen/Operatoren
+            ("Was macht +?", "Plus"),                   #addiert zwei Werte
+            ("Was macht -?", "Minus"),                  #subtrahiert Werte
+            ("Was macht *?", "Mal"),                    #multipliziert Werte
+            ("Was macht /?", "Geteilt"),                #teilt Werte
+            ("Was macht %?", "Rest"),                   #gibt den Rest einer Division
+            #GUI (tkinter)
+            ("Was ist tkinter?", "GUI"),                #bibliothek für benutzeroberflächen (Grafische Darstellung)
+            ("Was macht pack()?", "Stapel"),            #ordnet Elemente untereinander an
+            ("Was macht grid()?", "Raster"),            #ordnet Elemente in Tabellenform
+            ("Was macht place()?", "Position"),         #setzt Elemente an feste Position
+            #Entry / GUI Text
+            ("Was macht entry.get()?", "Lesen"),        #holt Text aus Eingabefeld
+            ("Was macht insert()?", "Schreiben"),       #fügt Text ein
+            ("Was macht delete()?", "Löschen"),         #löscht Text
+            ("Was ist entry?", "Feld"),                 #Eingabefeld in GUI
+        ]
     def create_player(self):
         self.frame_buttons.pack_forget()
         self.show_create_player_screen()
@@ -129,7 +197,12 @@ class quiz_game:
         self.frame_buttons.pack(expand=True)
 
     def play(self):
-        print("Spielen geklickt")
+        self.frame_buttons.pack_forget()
+        self.game()
+
+    def game(self):
+        self.frame_game = tkinter.Frame(self.root)
+        self.frame_game.pack()
 
     def show_score(self):
         self.frame_buttons.pack_forget()
